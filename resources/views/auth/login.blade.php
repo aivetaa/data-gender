5d6042bad36b2a7ecb8906fb5042c7cdaf14551b
aivetaa <122965618+aivetaa@users.noreply.github.com>
2023-08-28 22:15:35 +0700

    Update login.blade.php
    Adding unhide button for password, deleting some features (login via socials; FB and Google)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,84 +1,3 @@
-{{-- @extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
-
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
-
-                            <div class="row mb-3">
-                                <label for="email"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email"
-                                        class="form-control @error('email') is-invalid @enderror" name="email"
-                                        value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-                                <label for="password"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password"
-                                        class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="current-password">
-
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                            {{ old('remember') ? 'checked' : '' }}>
-
-                                        <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Login') }}
-                                    </button>
-
-                                    @if (Route::has('password.request'))
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                                            {{ __('Forgot Your Password?') }}
-                                        </a>
-                                    @endif
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endsection --}}
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -97,19 +16,6 @@
     <!-- Template CSS -->
     <link rel="stylesheet" href="../../admin/css/style.css">
     <link rel="stylesheet" href="../../admin/css/components.css">
-    <!-- Start GA -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'UA-94034622-3');
-    </script>
-    <!-- /END GA -->
 </head>
 
 <body>
@@ -136,24 +42,24 @@
                                         <label for="email">Email</label>
                                         <input id="email" type="email" class="form-control" name="email"
                                             tabindex="1" required autofocus>
-                                        <div class="invalid-feedback">
-                                            Please fill in your email
-                                        </div>
+                                        <!-- ... -->
                                     </div>
 
                                     <div class="form-group">
                                         <div class="d-block">
                                             <label for="password" class="control-label">Password</label>
-                                            <div class="float-right">
-                                                <a href="auth-forgot-password.html" class="text-small">
-                                                    Forgot Password?
-                                                </a>
+                                            <div class="input-group">
+                                                <input id="password" type="password" class="form-control" name="password"
+                                                    tabindex="2" required>
+                                                <div class="input-group-append">
+                                                    <button type="button" id="togglePassword" class="btn btn-secondary">
+                                                        <i class="fa fa-eye"></i>
+                                                    </button>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <input id="password" type="password" class="form-control" name="password"
-                                            tabindex="2" required>
-                                        <div class="invalid-feedback">
-                                            please fill in your password
+                                            <div class="invalid-feedback">
+                                                please fill in your password
+                                            </div>
                                         </div>
                                     </div>
 
@@ -173,28 +79,21 @@
                                     </div>
                                 </form>
                                 <div class="text-center mt-4 mb-3">
-                                    <div class="text-job text-muted">Login With Social</div>
+                                    <a href="{{ route('password.request') }}" class="text-small">
+                                        Forgot Password?
+                                    </a>
                                 </div>
                                 <div class="row sm-gutters">
-                                    <div class="col-6">
-                                        <a class="btn btn-block btn-social btn-facebook">
-                                            <span class="fab fa-facebook"></span> Facebook
-                                        </a>
-                                    </div>
-                                    <div class="col-6">
-                                        <a class="btn btn-block btn-social btn-twitter">
-                                            <span class="fab fa-twitter"></span> Twitter
-                                        </a>
-                                    </div>
+                                    <!-- ... -->
                                 </div>
 
                             </div>
                         </div>
                         <div class="mt-5 text-muted text-center">
-                            Don't have an account? <a href="auth-register.html">Create One</a>
+                            <!-- ... -->
                         </div>
                         <div class="simple-footer">
-                            Copyright &copy; Stisla 2018
+                            Copyright &copy; 2023. Diskominfo Nganjuk
                         </div>
                     </div>
                 </div>
@@ -207,10 +106,6 @@
     <script src="../../admin/modules/popper.js"></script>
     <script src="../../admin/modules/tooltip.js"></script>
     <script src="../../admin/modules/bootstrap/js/bootstrap.min.js"></script>
-    <script src="../../admin/modules/nicescroll/jquery.nicescroll.min.js"></script>
-    <script src="../../admin/modules/moment.min.js"></script>
-    <script src="../../admin/js/stisla.js"></script>
-
     <!-- JS Libraies -->
 
     <!-- Page Specific JS File -->
@@ -218,6 +113,21 @@
     <!-- Template JS File -->
     <script src="../../admin/js/scripts.js"></script>
     <script src="../../admin/js/custom.js"></script>
+    <!-- Script for toggle password -->
+    <script>
+        const togglePasswordButton = document.getElementById('togglePassword');
+        const passwordInput = document.getElementById('password');
+
+        togglePasswordButton.addEventListener('click', function() {
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                togglePasswordButton.innerHTML = '<i class="fa fa-eye-slash"></i>';
+            } else {
+                passwordInput.type = 'password';
+                togglePasswordButton.innerHTML = '<i class="fa fa-eye"></i>';
+            }
+        });
+    </script>
 </body>
 
 </html>
